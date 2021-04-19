32b5d970792bdbe487f0cbb1ee17756490281f9e
Bitpatroon <github@bitpatroon.nl>
2021-04-19 10:24:53 +0200

    Added extension dependency typo3

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,5 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email'   => 'typo3@bitpatroon.nl',
     'state'          => 'stable',
     'author_company' => 'Bitpatroon',
-    'version'        => '10.3',
+    'version'        => '10.4',
+    'constraints'    => [
+        'depends' => [
+            'typo3' => '10.4.0-10.9.9999',
+        ],
+    ],
 ];
